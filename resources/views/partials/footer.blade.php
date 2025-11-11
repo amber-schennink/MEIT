a86247615f672f7c8b7366cc29908946f65a9ce0
amber-schennink <a.schennink@deelnemer.ictmaatwerk.com>
2025-11-11 13:23:30 +0100

    Update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,20 +1,20 @@
 <footer class="bg-main-light mt-auto">
   <div class="max-w-[68rem] m-auto flex flex-col sm:flex-row gap-5 justify-between items-center p-8">
-    <a href="{{url('/')}}"><img class="w-[60px]" src="{{asset('assets/meit_logo.png')}}" /></a>
-    <div class="flex gap-3 footer-icons">
+    <a class="w-1/3" href="{{url('/')}}"><img class="w-[60px]" src="{{asset('assets/meit_logo.png')}}" /></a>
+    <div class="w-1/3 flex gap-3 footer-icons items-center justify-center">
       <a href="https://www.facebook.com/jacelyn.blok" target="_blank"><img src="{{asset('assets/facebook.svg')}}"/></a>
       <a href="https://www.linkedin.com/in/jacelyn-blok/" target="_blank"><img src="{{asset('assets/linkedin.svg')}}"/></a>
       <a href="https://www.instagram.com/meit.reizen/" target="_blank"><img src="{{asset('assets/insta.svg')}}"/></a>
       <a href="https://www.tiktok.com/@meit.reizen" target="_blank"><img src="{{asset('assets/tiktok.svg')}}"/></a>
       <a href="https://linktr.ee/MEIT.Links" target="_blank"><img src="{{asset('assets/linktree.svg')}}"/></a>
     </div>
-    <div class="flex flex-col items-center">
+    <div class="w-1/3 flex flex-col justify-end flex-wrap items-center">
       <a target="_blank" href="{{url('/algemene_voorwaarden_traject')}}" class="hover:underline underline-offset-2">Algemene voorwaarden</a>
       <a target="_blank" href="https://www.meit.nl/privacyverklaring" class="hover:underline underline-offset-2">Privacyverklaring</a>
     </div>
   </div>
   <div class="w-full h-0.5 bg-white"></div>
   <div class="p-2 m-auto w-fit">
-    <h6 class="text-[18px]"><span class="font-bold">©</span> 2025 MEIT | <a href="https://eazyonline.nl/" target="_blank">Design by <span class="text-[#05c2d4]">Eazy</span><span class="text-[#111b38]">online</span></a></h6>
+    <h6 class="text-[18px]"><span class="font-bold">©</span> 2025 MEIT | <a href="https://eazyonline.nl/" target="_blank">Design by <img class="inline h-[22px]" src="{{asset('assets/eazyonline_logo.png')}}" /></a></h6>
   </div>
 </footer>
