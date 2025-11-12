14f97801bd423dbed54f81643a4ec694c04caecc
amber-schennink <a.schennink@deelnemer.ictmaatwerk.com>
2025-11-12 14:51:01 +0100

    Update nav.blade.php

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -5,7 +5,7 @@
 @else
 <div class="relative h-[400px] text-black p-10 bg-[url('../../public/assets/meit_foto_boek.jpg')] bg-cover" style="background-position: 50% 40%;">
 @endif
-  <div class="hamburger relative z-40 md:hidden ml-auto w-fit" onclick="this.parentElement.classList.toggle('hamburger-active')">
+  <div class="hamburger relative z-50 md:hidden ml-auto w-fit" onclick="this.parentElement.classList.toggle('hamburger-active')">
     <div class="bar1"></div>
     <div class="bar2"></div>
     <div class="bar3"></div>
